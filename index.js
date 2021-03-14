5249b90ab7d7c8f2baa83e90df99c01e2d52ca8c
Bleak Chandler <bleakchandler@gmail.com>
2021-03-14 19:16:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const cards = [['Lisa', 'Kaitlin', 'Jan']];
+
+function writeCards(cards) {
+    let cards_array = [];
+    for (let i = 0; i < cards.length; i++) {
+      cards_array[i] = (`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+  
+    return cards_array;
+  }
+
+  writeCards(cards);
+
+
+function countDown(number){
+
+while (number > -1) {
+  console.log(number--);
+    } 
+}
+
+countDown(10);
